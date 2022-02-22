5c036d0d9f0ab50aceb74baaeae348a3f372336b
Azariel <frost.qc@gmail.com>
2022-02-22 12:42:36 -0500

    - Update message with more informative message on a specific error.

--- Custom/ProductsWatcher/ProductsHistory/Scraper/ProductTargetScraper.cs
+++ Custom/ProductsWatcher/ProductsHistory/Scraper/ProductTargetScraper.cs
@@ -561,7 +561,7 @@ this.Information: {Environment.NewLine}{JsonCommonSerializer.SerializeToString(t
                 string _ErrorMessage = $"Error. Price fetched from scrapper [{this.Information.Price}] is different from price fetched from DOM parser [{_Price}]. Price [{_Price}] will be considered as the right one. Abs Diff was [{Math.Abs(this.Information.Price.Value - _Price ?? 0)}].";
                 LoggingManager.LogToFile("3D62E30F-4D4D-4A64-8EC7-09C060D7D4AF", _ErrorMessage);
 
-                CreateErrorNotificationForAdministration($"<p>The price fetched was different from DOM parser price fetched.</p><p>this.OneOrMoreStepFailedAndRequiredAFallback = {this.OneOrMoreStepFailedAndRequiredAFallback}</p><p>_Price.HasValue={_Price.HasValue}</p><p>this.Information.Price.Value={this.Information.Price}</p><p>_Price.Value(by DOM only)={_Price}</p><p>Shipping Cost: {this.Information.ShippingCost}</p>");
+                CreateErrorNotificationForAdministration($"<p>The price fetched was different from DOM parser price fetched.</p><p>this.OneOrMoreStepFailedAndRequiredAFallback = {this.OneOrMoreStepFailedAndRequiredAFallback}</p><p>_Price.HasValue={_Price.HasValue}</p><p>this.Information.Price.Value={this.Information.Price}</p><p>_Price.Value(by DOM only)={_Price}</p><p>Shipping Cost: {this.Information.ShippingCost}</p><p>Assuming product price is {_Price}. If it's not, please update database accordingly.</p>");
 
                 // We consider DOM parser better than manual steps that can fail more easily. Ex: Amazon price could be $299.99 without "Price: " and we mismatch the next "Price: 314.14" we found. True case = https://www.amazon.ca/ADAM-Audio-Two-Way-Nearfield-Monitor/dp/B07B6JXBZH
                 this.Information.Price = _Price;
